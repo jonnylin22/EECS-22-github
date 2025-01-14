2b2af48aacca8d204ad86bfab0a2ba0a1ff3ffd8
Jonathan Lin <linjonathan22@gmail.com>
2025-01-14 00:44:33 -0800

    matrix.c assignment

---
+++ matrix.c
@@ -0,0 +1,76 @@
+/* 	matrix.c: 	multiplying two 2x2 matrices 		*/
+/* 	author: 	Jonathan Lin 						*/
+/* 	date: 		1/10/2025							*/
+
+#include <stdio.h>
+
+int main (void) {
+
+	/* variable declarations */
+	int a[] = {0,0,0,0}; 	// a[0]=a11 , a[1]=a12 , a[2]=a21 , a[3]=a22 
+	int b[] = {0,0,0,0}; 	// b[0]=b11 , b[1]=b12 , b[2]=b21 , b[3]=b22 
+	int c[]	= {0,0,0,0};	// resulting matrix from multiplication	
+	int index = 0;		// keep track of array index
+	int pos = 0;		// position within matrix	
+	char arr = 'a';
+	int printed = 0;
+	
+	/* 	Input section 	*/
+	printf("Enter the first matrix(a) that will be multiplied:\n");	
+	do {
+		if (index == 0 || index == 4){
+			pos = 11;	
+			if (index == 4) {
+				if (!printed) {
+					printf("\nEnter the second matrix(b) that will be multiplied:\n");
+					printed = 1;
+					arr = 'b';
+				}
+				
+			}
+		} else if (index == 1 || index == 5) {
+			pos = 12;
+		} else if (index == 2 || index == 6) {
+			pos = 21;
+
+		} else if (index == 3 || index == 7 ) {
+			pos = 22; 
+		}
+		printf("%c%d = ", arr, pos);
+
+		int num;
+		char term;
+		if(scanf("%d%c", &num, &term) != 2 || term != '\n') {
+			printf("ERROR: Invalid input. Please enter numbers only!\n");
+			while (getchar() != '\n'); 	// clear the input buffer 
+			continue;
+		}
+		else {
+			if (arr == 'a') {
+				a[index] = num;
+				index ++;
+			} else if (arr == 'b') {
+				b[index-4] = num;
+				index++;
+			}
+			
+		}
+
+
+	} while ( (index != 8) );  	// loop while the matrix has not yet been filled   
+
+	/* 	Compute 	*/
+	c[0] = a[0]*b[0] + a[1]*b[2];
+	c[1] = a[0]*b[1] + a[1]*b[3];
+	c[2] = a[2]*b[0] + a[3]*b[2];
+	c[3] = a[2]*b[1] + a[3]*b[3];	
+	/* 	Output 		*/
+	printf("\nResult:\n");
+	printf("%d ", c[0]);	
+	printf("%d", c[1]);
+	printf("\n%d ", c[2]);
+	printf("%d", c[3]);
+
+} /* End of main function */
+
+/* EOF */
